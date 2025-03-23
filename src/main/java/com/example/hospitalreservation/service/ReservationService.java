c6b7bf836552dc9bea9c0154d297e35fb8d67592
chunyj512 <chunyj512@gmail.com>
2025-03-23 10:58:29 +0900

    feat(service): 예약 서비스 로직 구현

--- src/main/java/com/example/hospitalreservation/service/ReservationService.java
+++ src/main/java/com/example/hospitalreservation/service/ReservationService.java
@@ -1,29 +1,40 @@
 package com.example.hospitalreservation.service;
 
 import com.example.hospitalreservation.model.Reservation;
+import com.example.hospitalreservation.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO : 서비스 레이어에서 필요한 어노테이션을 작성해주세요.
 @Service
 public class ReservationService {
-
     // TODO : 주입 받아야 객체를 작성해주세요.
+    private final ReservationRepository reservationRepository;
+    private Long idCounter = 1L;
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     // TODO : 모든 예약 리스트를 조회하는 코드를 작성해주세요.
     public List<Reservation> getAllReservations() {
-        return null;
+        return reservationRepository.findAll();
     }
 
     // TODO : 새로운 예약을 생성하는 코드를 작성해주세요.
     public Reservation createReservation(Long doctorId, Long patientId, LocalDateTime reservationTime) {
-        return null;
+        Reservation reservation = new Reservation();
+        reservation.setId(idCounter++);
+        reservation.setDoctorId(doctorId);
+        reservation.setPatientId(patientId);
+        reservation.setReservationTime(reservationTime);
+        return reservationRepository.save(reservation);
     }
 
     // TODO : 예약을 취소하는 코드를 작성해주세요.
     public void cancelReservation(Long id) {
-        return;
+        reservationRepository.deleteById(id);
     }
 }
