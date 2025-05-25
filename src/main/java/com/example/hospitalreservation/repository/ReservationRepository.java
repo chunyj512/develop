6416e711f6de4fa36ae1b4e68728765b090aa09c
chunyj512 <chunyj512@gmail.com>
2025-05-25 12:28:25 +0900

    refactor: ReservationRepository를 JPA 인터페이스로 변경

--- src/main/java/com/example/hospitalreservation/repository/ReservationRepository.java
+++ src/main/java/com/example/hospitalreservation/repository/ReservationRepository.java
@@ -3,12 +3,12 @@ package com.example.hospitalreservation.repository;
 import com.example.hospitalreservation.model.Reservation;
 import org.springframework.stereotype.Repository;
 import com.example.hospitalreservation.dto.ReservationDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
-public class ReservationRepository {
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     private final List<Reservation> reservations = new ArrayList<>();
     private Long nextId = 1L;
